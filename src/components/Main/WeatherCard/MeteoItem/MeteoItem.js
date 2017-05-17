2d57834eae9573f1caed54bc514b2957c5f9c631
Gralex <aleksandr.grigorchuk@gmail.com>
2017-05-17 16:32:57 +0300

    Apply style WeatherCard component

--- src/components/Main/WeatherCard/MeteoItem/MeteoItem.js
+++ src/components/Main/WeatherCard/MeteoItem/MeteoItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './MeteoItem.css';
 
 const MeteoItem = props => {
 	const { type, label, value, unit } = props;
--- src/components/Main/WeatherCard/WeatherCard.js
+++ src/components/Main/WeatherCard/WeatherCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MeteoItem } from './MeteoItem/MeteoItem';
 import { Time } from './Time/Time';
 import { Station } from './Station/Station';
+import './WeatherCard.css';
 
 const WeatherCard = props => {
 	const { weather } = props;
@@ -9,12 +10,12 @@ const WeatherCard = props => {
 	return (
 		<section className="weather-card">
 			<Time time={weather.time}/>
-			<MeteoItem label={weather.lat.label} value={weather.lat.value} unit={weather.lat.unit}/>
-			<MeteoItem label={weather.lon.label} value={weather.lon.value} unit={weather.lon.unit}/>
-			<MeteoItem icon={weather.temperature.icon} label={weather.temperature.label} value={weather.temperature.value} unit={weather.temperature.unit}/>
-			<MeteoItem icon={weather.pressure.icon} label={weather.pressure.label} value={weather.pressure.value} unit={weather.pressure.unit}/>
-			<MeteoItem icon={weather.wind.icon} label={weather.wind.label} value={weather.wind.value} unit={weather.wind.unit}/>
-			<MeteoItem icon={weather.humidity.icon} label={weather.humidity.label} value={weather.humidity.value} unit={weather.humidity.unit}/>
+			<MeteoItem type={weather.lat.type} label={weather.lat.label} value={weather.lat.value} unit={weather.lat.unit}/>
+			<MeteoItem type={weather.lon.type} label={weather.lon.label} value={weather.lon.value} unit={weather.lon.unit}/>
+			<MeteoItem type={weather.temperature.type} label={weather.temperature.label} value={weather.temperature.value} unit={weather.temperature.unit}/>
+			<MeteoItem type={weather.pressure.type} label={weather.pressure.label} value={weather.pressure.value} unit={weather.pressure.unit}/>
+			<MeteoItem type={weather.wind.type} label={weather.wind.label} value={weather.wind.value} unit={weather.wind.unit}/>
+			<MeteoItem type={weather.humidity.type} label={weather.humidity.label} value={weather.humidity.value} unit={weather.humidity.unit}/>
 			<Station name={weather.base}/>
 		</section>
 	);
